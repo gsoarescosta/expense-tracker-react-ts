f4c82a7aec7bb82a77f611693e6848d60edaf6ff
Gabriel Soares Costa <gabrielsoarescosta.gsc@gmail.com>
2021-10-13 13:13:37 -0300

    Added photos section and integration with Firebase

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -10,13 +10,58 @@ export const Header = styled.div`
 
 export const HeaderText = styled.h1`
   margin: 0;
-  padding: 0;
+  padding: 30px 0 0 0;
   color: #ffffff;
-  padding-top: 30px;
 `;
 
 export const Body = styled.div`
   margin: auto;
   max-width: 980px;
   margin-bottom: 50px;
+`;
+
+export const PhotoHeaderText = styled.h1`
+  margin: 0;
+  padding: 20px 0;
+  color: #888;
+  text-align: center;
+`;
+
+export const ScreenWarning = styled.div`
+  text-align: center;
+
+  .emoji {
+    font-size: 50px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const PhotoList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+`;
+
+export const UploadForm = styled.form`
+  background-color: #f3f3f3;
+  padding: 15px;
+  border-radius: 5px;
+  margin-bottom: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  input[type="submit"] {
+    background-color: lightblue;
+    border: 0;
+    color: black;
+    padding: 8px 16px;
+    font-size: 15px;
+    border-radius: 10px;
+    margin: 0 20px;
+    cursor: pointer;
+    &:hover {
+      background-color: blue;
+      color: white;
+    }
+  }
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import * as C from "./App.styles";
 import { Item } from "./types/Item";
 // import { Category } from "./types/Category";
@@ -8,6 +8,9 @@ import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
 import { TableArea } from "./components/TableArea";
 import { InfoArea } from "./components/InfoArea";
 import { InputArea } from "./components/InputArea";
+import * as Photos from "./services/photos";
+import { Photo } from "./types/Photo";
+import { PhotoItem } from "./components/PhotoItem";
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -45,22 +48,106 @@ const App = () => {
     setList(newList);
   };
 
+  /* Photos area */
+  const [uploading, setUploading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    getPhotos();
+    console.log(photos.length);
+    if (photos.length > 0) {
+      console.log(photos[0].name);
+      console.log(photos[0].url);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getPhotos = async () => {
+    setLoading(true);
+    setPhotos(await Photos.getAll());
+    setLoading(false);
+  };
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("image") as File;
+
+    if (file && file.size > 0) {
+      setUploading(true);
+      let result = await Photos.insert(file);
+      setUploading(false);
+
+      if (result instanceof Error) {
+        alert(`${result.name} - ${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
+  };
+
+  const handleDeleteClick = async (name: string) => {
+    await Photos.deletePhoto(name);
+    getPhotos();
+  };
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        <InfoArea
-          currentMonth={currentMonth}
-          onMonthChange={handleMonthChange}
-          income={income}
-          expense={expense}
-        />
+        {loading && (
+          <C.ScreenWarning>
+            <div className="emoji">⌛</div>
+            <div>Carregando ...</div>
+          </C.ScreenWarning>
+        )}
 
-        <InputArea onAdd={handleAddItem} />
+        {!loading && (
+          <>
+            <InfoArea
+              currentMonth={currentMonth}
+              onMonthChange={handleMonthChange}
+              income={income}
+              expense={expense}
+            />
+            <InputArea onAdd={handleAddItem} />
+            <TableArea list={filteredList} />
+            <C.PhotoHeaderText>Fotos (contas/recibos)</C.PhotoHeaderText>
+            <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
+              <input type="file" name="image" />
+              <input type="submit" value="Enviar" />
+              {uploading && "Enviando..."}
+            </C.UploadForm>
+          </>
+        )}
 
-        <TableArea list={filteredList} />
+        {!loading && photos.length > 0 && (
+          <C.PhotoList>
+            {photos.map((item, index) => {
+              return (
+                <PhotoItem
+                  key={index}
+                  url={item.url}
+                  name={item.name}
+                  onDelete={handleDeleteClick}
+                />
+              );
+            })}
+          </C.PhotoList>
+        )}
+
+        {!loading && photos.length === 0 && (
+          <C.ScreenWarning>
+            <div className="emoji">🤡</div>
+            <div>Não há fotos cadastradas.</div>
+          </C.ScreenWarning>
+        )}
       </C.Body>
     </C.Container>
   );
